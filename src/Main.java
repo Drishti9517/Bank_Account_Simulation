507c1895fe0959d23f63e029ac13da87746618ac
Drishti Sahu <drishtisahu2007@gmail.com>
2026-09-16 00:38:35 +0530

    formatting

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
+        System.out.println("===== BANK ACCOUNT SIMULATION =====");
         Scanner in = new Scanner(System.in);
         System.out.print("Enter account holder name: ");
         String holderName = in.nextLine();
@@ -13,11 +14,12 @@ public class Main{
         float out = in.nextFloat();
 
         Account acc = new Account(holderName, Account.accNo(1000), Account.deposit(initial_deposit));
-
-        System.out.println("---------------Account created!---------------");
+        System.out.println();
+        System.out.println("Account created!");
 
         System.out.println("Account holder's name: " + holderName);
         System.out.println("Account No.- " + Account.accNo(1000));
+        System.out.println();
         System.out.println("Deposited:- " + Account.deposit(initial_deposit));
         System.out.println("Money withdraw: " + Account.withdraw(out));
         System.out.println("Balance: " + Account.balance());
